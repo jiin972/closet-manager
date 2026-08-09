7d73d838488b235dd760993f67502ac0d81c5f76
jiin972 <jiin972@gmail.com>
2026-08-10 07:42:18 +0900

    chore: update initial project setting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //@ts-ignore
+  allowedDevOrigins: ["100.127.64.40"],
 };
 
 export default nextConfig;
