047c313db88c2817a1dd97eae1e0a4d77bcdf46c
jiin972 <jiin972@gmail.com>
2026-08-15 06:39:12 +0900

    feat: implement reuseble input components

---
+++ components/auth_input.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+interface AuthInputProps {
+  name: string;
+  errors?: string[];
+}
+
+/**
+ * 재사용이 가능한 Input Components
+ * - React 기본 Input속성을 모두 그대로 지원(...rest)
+ * - 필드별 에러 메시지를 input 아래에 함께 렌더링
+ */
+export default function AuthInput({
+  name,
+  errors = [],
+  ...rest
+}: AuthInputProps & React.InputHTMLAttributes<HTMLInputElement>) {
+  const hasError = errors.length > 0;
+  return (
+    <div className="flex flex-col gap-1 w-full">
+      <input
+        name={name}
+        {...rest}
+        className={`w-full rounded-lg border border-white px-4 py-2 placeholder:text-gray-400 focus:outline-none focus:border-2 transition-all
+          ${hasError ? "border-red-500" : "border-white focus:border-white"}`}
+      />
+      {errors?.map((error, index) => (
+        <span key={index} className="text-red-400 text-sm">
+          {error}
+        </span>
+      ))}
+    </div>
+  );
+}
