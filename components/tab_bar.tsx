ad60cede6809ba51d64527d51f0b84cbce18894b
jiin972 <jiin972@gmail.com>
2026-08-19 15:02:15 +0900

    refactor: update button layout - adjust button order - update button labels - add 'add-item'button

--- components/tab_bar.tsx
+++ components/tab_bar.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 import {
-  UserIcon,
-  CalendarDaysIcon,
-  HomeIcon,
-} from "@heroicons/react/24/solid";
-import {
   UserIcon as OutLineUserIcon,
-  CalendarDaysIcon as OutlineCalrendar,
+  InboxStackIcon as OutlienInboxStack,
   HomeIcon as OutlineHome,
+  SwatchIcon as OutlineSwatch,
 } from "@heroicons/react/24/outline";
+import {
+  HomeIcon,
+  InboxStackIcon,
+  PlusIcon,
+  SwatchIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function TabBar() {
   const pathname = usePathname();
   return (
-    <div className="fixed bottom-0 w-full max-w-3xl mx-auto grid grid-cols-3 justify-items-center py-3 bg-tab-bar *:text-white ">
+    <div className=" fixed bottom-0 w-full max-w-3xl mx-auto grid grid-cols-5 justify-items-center py-3 bg-tab-bar *:text-white ">
       <Link href="/home" className="flex flex-col items-center gap-px">
         {pathname === "/home" ? (
           <HomeIcon className="w-6 h-6" />
@@ -25,13 +28,29 @@ export default function TabBar() {
         )}
         <span className="text-xs">홈</span>
       </Link>
-      <Link href="/diary" className="flex flex-col items-center gap-px">
-        {pathname === "/diary" ? (
-          <CalendarDaysIcon className="w-6 h-6" />
+      <Link href="/closet" className="flex flex-col items-center gap-px">
+        {pathname === "/closet" ? (
+          <InboxStackIcon className="w-6 h-6" />
         ) : (
-          <OutlineCalrendar className="w-6 h-6" />
+          <OutlienInboxStack className="w-6 h-6" />
         )}
-        <span className="text-xs">다이어리</span>
+        <span className="text-xs">옷장</span>
+      </Link>
+      <Link
+        href="/add-item"
+        className="relative flex flex-col items-center gap-px"
+      >
+        <div className="absolute -top-4 flex items-center justify-center w-12 h-12 rounded-full bg-gray-50 text-black hover:bg-orange-900 hover:text-white">
+          <PlusIcon className="w-6 h-6" />
+        </div>
+      </Link>
+      <Link href="/coordination" className="flex flex-col items-center gap-px">
+        {pathname === "/coordination" ? (
+          <SwatchIcon className="w-6 h-6" />
+        ) : (
+          <OutlineSwatch className="w-6 h-6" />
+        )}
+        <span className="text-xs">코디</span>
       </Link>
       <Link href="/profile" className="flex flex-col items-center gap-px">
         {pathname === "/profile" ? (
