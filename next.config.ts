e4cc9311eb279c631ca04c5fa6dd95319861314b
jiin972 <jiin972@gmail.com>
2026-09-18 23:03:32 +0900

    feat: add Tailscale HTTPS domain to DevOrigns for Geolocation API

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   //@ts-ignore
-  allowedDevOrigins: ["100.127.64.40"],
+  allowedDevOrigins: ["100.127.64.40", "jiin972-macmini.taildeb984.ts.net"],
 };
 
 export default nextConfig;
